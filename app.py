dd40e64255ce2b69e0816c14ff2dde32d5a5428e
Mahi Patel <mahi@Mahis-MacBook-Air.local>
2026-05-17 15:42:45 -0500

    Fix Google redirect URI for production

--- app.py
+++ app.py
@@ -17,8 +17,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 GOOGLE_CLIENT_ID = os.getenv('GOOGLE_CLIENT_ID')
 GOOGLE_CLIENT_SECRET = os.getenv('GOOGLE_CLIENT_SECRET')
-GOOGLE_REDIRECT_URI = 'http://127.0.0.1:5000/auth/google/callback'
-
+GOOGLE_REDIRECT_URI = os.getenv('GOOGLE_REDIRECT_URI', 'http://127.0.0.1:5000/auth/google/callback')
 db.init_app(app)
 
 login_manager = LoginManager()
